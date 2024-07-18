dc3c0167029f535ebeb8b90fc8d9cd8dd3f5155b
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-07-19 02:20:07 +0530

    Back end Completed

---
+++ client/src/app/admin/login/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Adminlogin</div>
+  )
+}
+
+export default page
---
+++ client/src/componeents/Herder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Herder = () => {
+  return (
+    <div>Herder</div>
+  )
+}
+
+export default Herder
